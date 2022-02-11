633d70ffabe269a9df456b3c7143be2b9c7e7376
TEAM 107 <107scoutingdata@gmail.com>
2022-02-10 19:06:53 -0500

    Updated to Flight controller joystick

--- src/main/java/frc/robot/commands/SwerveDriveCommand.java
+++ src/main/java/frc/robot/commands/SwerveDriveCommand.java
@@ -37,15 +37,27 @@ public class SwerveDriveCommand extends CommandBase {
     SmartDashboard.putNumber("Y channel", FlightController.getY());
     // Get the x speed. We are inverting this because Xbox controllers return
     // negative values when we push forward.
+    double X = -FlightController.getX();
+    if (Math.abs(X)<0.05){
+      X = 0;
+    }
+    double Y = FlightController.getY();
+    if (Math.abs(Y)<0.05){
+      Y = 0;
+    }
+    double Z = FlightController.getZ();
+    if (Math.abs(Z)<0.05){
+      Z = 0;
+    }
     final var xSpeed =
-      xspeedLimiter.calculate(FlightController.getY())
+      xspeedLimiter.calculate(Y)
         * DriveConstants.kMaxSpeedMetersPerSecond;
-
-    // Get the y speed or sideways/strafe speed. We are inverting this because
+    
+   // Get the y speed or sideways/strafe speed. We are inverting this because
     // we want a positive value when we pull to the left. Xbox controllers
     // return positive values when you pull to the right by default.
     final var ySpeed =
-      yspeedLimiter.calculate(FlightController.getX())
+      yspeedLimiter.calculate(X)
         * DriveConstants.kMaxSpeedMetersPerSecond;
 
     // Get the rate of angular rotation. We are inverting this because we want a
@@ -53,7 +65,7 @@ public class SwerveDriveCommand extends CommandBase {
     // mathematics). Xbox controllers return positive values when you pull to
     // the right by default.
     final var rot =
-       rotLimiter.calculate(FlightController.getZ())
+       rotLimiter.calculate(Z)
          * DriveConstants.kMaxSpeedMetersPerSecond;
     // final var rot =
     //     -rotLimiter.calculate(0) //leftController.getZ())
