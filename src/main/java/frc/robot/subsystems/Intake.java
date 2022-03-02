0c5daf9cb0dd050cd4a5702342d507be1a08f8fc
TEAM 107 <107scoutingdata@gmail.com>
2022-03-01 19:35:52 -0500

    Setup Intake system with PID position control

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -12,8 +12,11 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.Motors;
 
+import java.util.ResourceBundle.Control;
+
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.InvertType;
+import com.ctre.phoenix.motorcontrol.NeutralMode;
 // import com.ctre.phoenix.motorcontrol.ControlMode;
 // import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
@@ -26,13 +29,13 @@ public class Intake extends SubsystemBase {
   
   public static final class IntakeArmConstants {
     //TODO run arm motor to extended position, find right position
-    public static final double armLowerLimit = 0;
-    public static final double armUpperLimit = 0;
+    public static final double armRetractedPos = 10000;// 535000;
+    public static final double armExtendedPos = 0;
     
-    //TODO tune the intake arm PID values
-    public static final double kP = 0.25; 
-    public static final double kI = 0.0005;
-    public static final double kD = 0.0001; 
+    // intake arm PID values
+    public static final double kP = 0.04; 
+    public static final double kI = 0.0001;
+    public static final double kD = 0.0; 
     public static final double kIz = 8000; 
     public static final double kFF = 0;//.000015; 
     // public static final double kMaxOutput = 1; 
@@ -42,15 +45,14 @@ public class Intake extends SubsystemBase {
 
   public static final class IntakeMotorConstants {
 
-    public static final double kP = 0.03; 
-    public static final double kI = 0;
+    public static final double kP = 0.04; 
+    public static final double kI = 0.00;
     public static final double kD = 0; 
-    public static final double kIz = 0; 
+    public static final double kIz = 2000; 
     public static final double kFF = 0;//.000015; 
     // public static final double kMaxOutput = 1; 
     // public static final double kMinOutput = -1;
     // public static final double maxRPM = 5700; 
-
 
   }
   /**
@@ -60,12 +62,17 @@ public class Intake extends SubsystemBase {
     m_IntakeMotor = new WPI_TalonSRX(Motors.BALL_INTAKE);
     m_IntakeMotor.configFactoryDefault();
     m_IntakeMotor.setInverted(false);
-    m_IntakeMotor.configOpenloopRamp(0.2);
+    m_IntakeMotor.configClosedloopRamp(0.5);
 
     
+    m_IntakeMotor.config_kP(0, IntakeMotorConstants.kP);
+    m_IntakeMotor.config_kI(0, IntakeMotorConstants.kI);
+    m_IntakeMotor.config_kD(0, IntakeMotorConstants.kD);
+    m_IntakeMotor.config_IntegralZone(0, IntakeMotorConstants.kIz);
+    m_IntakeMotor.config_kF(0, IntakeMotorConstants.kFF);
 
-    double junk = SmartDashboard.getNumber("intakeSpeed", 0.20);
-    SmartDashboard.putNumber("intakeSpeed", junk);
+    //double junk = SmartDashboard.getNumber("intakeSpeed", 0.20);
+    //SmartDashboard.putNumber("intakeSpeed", junk);
 
     m_CurrentSpeed = 0;
    
@@ -73,26 +80,21 @@ public class Intake extends SubsystemBase {
     m_IntakeArm = new WPI_TalonSRX(Motors.INTAKE_ARM);
     m_IntakeArm.configFactoryDefault();
     m_IntakeArm.setInverted(false);
-    m_IntakeArm.setSelectedSensorPosition(0);
-    //TODO set PID values for INTAKE_ARM
+    m_IntakeArm.setNeutralMode(NeutralMode.Brake);
+    m_IntakeArm.setSelectedSensorPosition(IntakeArmConstants.armRetractedPos);
+    // PID values for INTAKE_ARM
     m_IntakeArm.config_kP(0, IntakeArmConstants.kP);
     m_IntakeArm.config_kI(0, IntakeArmConstants.kI);
     m_IntakeArm.config_kD(0, IntakeArmConstants.kD);
     m_IntakeArm.config_IntegralZone(0, IntakeArmConstants.kIz);
     m_IntakeArm.config_kF(0, IntakeArmConstants.kFF);
 
-    m_IntakeArm.setSelectedSensorPosition(IntakeArmConstants.armLowerLimit);
+
+    m_IntakeArm.setSelectedSensorPosition(IntakeArmConstants.armRetractedPos);
     // m_IntakeArm.configClearPositionOnLimitR(clearPositionOnLimitR, timeoutMs)
     // m_IntakeArm.configClearPositionOnLimitF(clearPositionOnLimitF, timeoutMs)
-    //m_IntakeArm.get
 
     intakeExtended = false;
-
-    m_IntakeMotor.config_kP(0, IntakeMotorConstants.kP);
-    m_IntakeMotor.config_kI(0, IntakeMotorConstants.kI);
-    m_IntakeMotor.config_kD(0, IntakeMotorConstants.kD);
-    m_IntakeMotor.config_IntegralZone(0, IntakeMotorConstants.kIz);
-    m_IntakeMotor.config_kF(0, IntakeMotorConstants.kFF);
   }
 
   @Override
@@ -102,21 +104,33 @@ public class Intake extends SubsystemBase {
     // runMotor(m_CurrentSpeed);
         //m_IntakeMotor.set(ControlMode.PercentOutput, m_CurrentSpeed);
     m_IntakeMotor.set(ControlMode.Velocity, m_CurrentSpeed);
-
+SmartDashboard.putNumber("IntakeArmAt", m_IntakeArm.getSelectedSensorPosition());
     // if upper or lower limit switch is hit, then reset encoder position to upper or lower
     if (m_IntakeArm.getSensorCollection().isFwdLimitSwitchClosed()){
-      m_IntakeArm.setSelectedSensorPosition(IntakeArmConstants.armUpperLimit);
+      m_IntakeArm.setSelectedSensorPosition(IntakeArmConstants.armRetractedPos);
     }
     else if (m_IntakeArm.getSensorCollection().isRevLimitSwitchClosed()){
-      m_IntakeArm.setSelectedSensorPosition(IntakeArmConstants.armLowerLimit);
+      m_IntakeArm.setSelectedSensorPosition(IntakeArmConstants.armExtendedPos);
     }
   }
   // public void runMotor(double speed){
   //   //m_IntakeMotor.set(ControlMode.PercentOutput, speed);
   //   m_IntakeMotor.set(ControlMode.Velocity, speed);
   // }
+public void extendArm(){
+  // m_IntakeArm.set(ControlMode.PercentOutput, 1);
+  m_IntakeArm.set(ControlMode.Position, IntakeArmConstants.armExtendedPos);
+}
+public void retractArm(){
+  //m_IntakeArm.set(ControlMode.PercentOutput, -1);
+  m_IntakeArm.set(ControlMode.Position, IntakeArmConstants.armRetractedPos);
+}
 
-  public void ToggleIntake(){
+public void stopArm() {
+  m_IntakeArm.set(ControlMode.PercentOutput, 0);
+}
+
+public void ToggleIntake(){
     if (!intakeExtended){
       intakeExtended = true;
       //m_IntakeArm.set(ControlMode.Position, IntakeArmConstants.armUpperLimit);
@@ -129,7 +143,6 @@ public class Intake extends SubsystemBase {
       //m_IntakeArm.set(ControlMode.Position, IntakeArmConstants.armLowerLimit);
       m_CurrentSpeed = 0;
     }
-
   }
 
   public void HeimlichManeuver() {
